8582fa9e89f9e7904a049d61d86d7607985a8a60
Jboumalh8 <121645626+Jboumalh8@users.noreply.github.com>
2023-01-26 11:43:20 -0500

    Add files via upload

---
+++ DC_MOTOR_Script.m
@@ -0,0 +1,10 @@
+% Simulate DC Motor
+
+V = 1; 
+J = 3.2284E-6;  %kg m^2
+b = 3.5077E-6;  %Nms
+Kb = 0.0274;    %V/rad/s
+Kt = 0.0274;    %Nm/Amp
+R = 4;          %ohms
+L = 2.75E-6;    %H
+sim('DC_MOTOR_SIMULINK.slx')
